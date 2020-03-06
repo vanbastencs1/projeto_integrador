3347cfa9d87e24fb1bc40c239b16261298f3798d
Mikael Santos <mikael.ffc@gmail.com>
2020-03-06 19:11:17 -0300

    Adicionando o Index

--- projeto1/index.php
+++ projeto1/index.php
@@ -1,83 +1,91 @@
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="bkg.css">
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
     <title>Olá, mundo!</title>
-    <link rel="stylesheet" href="css/estilo.css">
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a class="navbar-brand" href="index.php">Projeto</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item">
-                <a class="nav-link active" href="formulario.php">Compra</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" href="login.php">Login</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" href="cadastro.php">Cadastro</a>
-              </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-              <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
-              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
-            </form>
+  <div class="logo">
+          <a href="index.php">
+          <img style="max-width:110px; margin-top: -9px;" src="images/letra.png">
           </div>
-        </nav>
-  <div class="wrapper container">
-			<div class="inner">
-				<div class="image-holder">				
-				</div>
-        <div class="container">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-
-                <div class="nav-scroller py-1 mb-2">
-                </div>
-          
-                <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(página atual)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="cadastro.php">Cadastro</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Ação</a>
+          <a class="dropdown-item" href="#">Outra ação</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Algo mais aqui</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Desativado</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+    </form>
+  </div>
+</nav>
+    <div class="container">
+    <div class="nav-scroller py-2 mb-0">
+    <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
                   <div class="col-md-6 px-0">
-                    <h1 class="display-4 font-italic">Título mais longo para um post destacado</h1>
-                    <p class="lead my-3">Várias linhas formando uma introdução, informando novos leitores rápido e eficientemente sobre o que é mais interessante, neste post.</p>
-                    <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue lendo...</a></p>
+                    <h1 class="display-4 font-italic">40 Cervejas artesanais brasileiras que você precisa beber</h1>
+                    <p class="lead my-3">Silenciosamente, acontece uma revolução cervejeira no Brasil. A cada dia, novas cervejas artesanais são criadas para atender um público crescente e sedento por novidades.</p>
+                    <p class="lead mb-0"><a href="https://manualdohomemmoderno.com.br/cerveja/40-cervejas-artesanais-brasileiras-que-voce-precisa-beber" class="text-white font-weight-bold">Continue lendo...</a></p>
                   </div>
                 </div>
-          
                 <div class="row mb-2">
                   <div class="col-md-6">
                     <div class="card flex-md-row mb-4 shadow-sm h-md-250">
                       <div class="card-body d-flex flex-column align-items-start">
                         <strong class="d-inline-block mb-2 text-primary">Mundo</strong>
                         <h3 class="mb-0">
-                          <a class="text-dark" href="#">Post destacado</a>
+                          <a class="text-dark" href="#">100 Cervejas Especiais que você precisa beber</a>
                         </h3>
                         <div class="mb-1 text-muted">12 de Nov</div>
-                        <p class="card-text mb-auto">Este é um card maior com suporte à texto, em baixo, como uma introdução mais natural ao conteúdo adicional.</p>
-                        <a href="#">Continue lendo</a>
+                        <p class="card-text mb-auto">Para quem curte o fermentado de lúpulo e cevada, sabe que a cerveja pode agradar os mais diversos paladares, situações e clima, basta conhecer o rótulo (ou estilo) certo.</p>
+                        <a href="https://manualdohomemmoderno.com.br/cerveja/100-cervejas-especiais-que-voce-precisa-beber">Continue lendo</a>
                       </div>
-                      <img class="card-img-right flex-auto d-none d-lg-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
+                      <img class="card-img-right flex-auto d-none d-lg-block" src="100cerva.png" alt="Card image cap">
                     </div>
                   </div>
                   <div class="col-md-6">
                     <div class="card flex-md-row mb-4 shadow-sm h-md-250">
                       <div class="card-body d-flex flex-column align-items-start">
-                        <strong class="d-inline-block mb-2 text-success">Design</strong>
+                        <strong class="d-inline-block mb-2 text-success">Ressaca</strong>
                         <h3 class="mb-0">
-                          <a class="text-dark" href="#">Título do post</a>
+                          <a class="text-dark" href="#">Cientistas descobrem a cura para a ressaca</a>
                         </h3>
                         <div class="mb-1 text-muted">11 de Nov</div>
-                        <p class="card-text mb-auto">Este é um card maior com suporte à texto em baixo, como uma introdução mais natural ao conteúdo adicional.</p>
-                        <a href="#">Continue lendo</a>
+                        <p class="card-text mb-auto"> Muitos cientistas se aplicaram pela causa de muitos males, mas pouco foi descoberto até agora. Isso porque pesquisadores chineses analisaram o problema de um novo ângulo, como a ressaca pode ser tratada antes mesmo de começar e alegam ter encontrado a resposta. Ela se chama Sprite.</p>
+                        <a href="https://manualdohomemmoderno.com.br/cerveja/cientistas-descobrem-cura-ressaca">Continue lendo</a>
                       </div>
-                      <img class="card-img-right flex-auto d-none d-lg-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
+                      <img class="card-img-right flex-auto d-none d-lg-block" src="homer.jpg" alt="Card image cap">
                     </div>
                   </div>
                 </div>
@@ -87,20 +95,20 @@
                 <div class="row">
                   <div class="col-md-8 blog-main">
                     <h3 class="pb-3 mb-4 font-italic border-bottom">
-                      O dossiê!
+                      
                     </h3>
           
                     <div class="blog-post">
-                      <h2 class="blog-post-title">Exemplo de post</h2>
-                      <p class="blog-post-meta">Escrito por <a href="#">Mark</a>, em 1 de Janeiro de 2018.</p>
+                      <h2 class="blog-post-title">Cerveja em gel é lançada nos EUA</h2>
+                      <p class="blog-post-meta">Escrito por Sabrina</a>, em 1 de Janeiro de 2018.</p>
           
-                      <p>Este post mostra alguns diferentes tipos de conteúdos que são suportados e estilizados com Bootstrap. Tipografia básica, imagens e códigos são todos suportados.</p>
+                      <p>Cerveja é bom, mas o grande problema do líquido de cevada é ter que carregar latas ou engradados quando você viaja, pega uma trilha ou mesmo anda de bicicleta. A empresa Pat’s Backcountry Beverage, do Alasca, EUA, quer resolver esse problema com um concentrado de cerveja em gel que, adicionando água, vira nada menos do que uma cerveja, com gás e tudo.</p>
                       <hr>
-                      <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
+                      <p>De acordo com a marca, a Pale Rail Brew Concentrate funciona como esses sucos de saquinho. Ela promete o sabor, aroma e teor alcoólico igual aos de uma cerveja normal</p>
                       <blockquote>
-                        <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                        <p>A marca lançou duas novas linhas do gel concentrado de cerveja com sabores mais sofisticados. A nova linha consiste de dois sabores : Black Rops (cerveja negra) e Pale Trail (cerveja clara). O produto contém álcool, com direito a colarinho e aroma.</p>
                       </blockquote>
-                      <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+                      <p>Quem pensa que a fabrica desidratou a cerveja, errou. No site, eles descrevem o processo como um controle cuidadoso de fermentação do produto, usando o mínimo de água possível. Para consumir, basta adicionar água e ‘carbonatar’ – acrescentar o gás – que eles mesmos vendem um produto portátil pra isso. Confira o vídeo do squeeze especial dos caras.</p>
                       <h2>Heading</h2>
                       <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
                       <h3>Sub-heading</h3>
@@ -149,10 +157,6 @@
                       <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
                     </div><!-- /.blog-post -->
           
-                    <nav class="blog-pagination">
-                      <a class="btn btn-outline-primary" href="#">Mais antigos</a>
-                      <a class="btn btn-outline-secondary disabled" href="#">Mais novos</a>
-                    </nav>
           
                   </div><!-- /.blog-main -->
           
@@ -176,26 +180,21 @@
                 </div><!-- /.row -->
           
               </main><!-- /.container -->
-              <br>
+    </div>
+    </div>
+    <div class="footer">
               <footer>
-                <p>Template de blog construído para o <a href="https://getbootstrap.com/">Bootstrap</a>, pelo <a href="https://twitter.com/mdo">@mdo</a>.</p>
+                <p>Template de blog construído para o <a href="https://getbootstrap.com/">Bootstrap</a></p>
                 <p>
-                  <a href="#">Voltar ao topo</a>
+                  <a href="index.php">Voltar ao topo</a>
                 </p>
+                <div class="inner_footer">
+                    <img style="max-width:150px; margin: -125px 0 -55px 0; float: center" src="images/letra.png">
+                </div>
               </footer>
-          
-              <!-- Principal JavaScript do Bootstrap
-              ================================================== -->
-              <!-- Foi colocado no final para a página carregar mais rápido -->
-              <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-              <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-              <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-              <script>
-                Holder.addTheme('thumb', {
-                  bg: '#55595c',
-                  fg: '#eceeef',
-                  text: 'Thumbnail'
-                });
-              </script>
+              </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   </body>
 </html>
